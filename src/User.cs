2c8fd0d57416dc0bc2e34d4b35364293df4b98cf
Kabanosk <fiolkawojciech@gmail.com>
2022-04-06 01:03:27 +0200

    Add class for User and some first functions

--- src/User.cs
+++ src/User.cs
@@ -2,6 +2,20 @@
 {
     public class User
     {
-        
+        private string Name { get; set; }
+        private string Email { get; set; }
+
+        public User(string name, string email)
+        {
+            Name = name;
+            Email = email;
+        }
+        public override string ToString()
+        {
+            return string.Format("User:\n" +
+                                 "\tName: {0}\n" +
+                                 "\tEmail: {1}", Name, Email);
+        }
+
     }
 }
