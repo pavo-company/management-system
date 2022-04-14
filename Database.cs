785850e7bd4addced45212960c21f8586985c55b
Kabanosk <fiolkawojciech@gmail.com>
2022-04-14 14:28:23 +0200

    Creating tables when needed

--- Database.cs
+++ Database.cs
@@ -1,4 +1,6 @@
-﻿using System.Data.SQLite;
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
 using System.IO;
 
 namespace management_system
@@ -13,8 +15,32 @@ namespace management_system
             {
                 SQLiteConnection.CreateFile("../../../database.sqlite");
             }
-
+            string createUserTable = "create table users (id INTEGER, name TEXT, surname TEXT, email TEXT);";
+            string createWorkersTable =
+                "create table workers (id INTEGER, name TEXT, surname TEXT, salary INTEGER, email TEXT);";
             Connection = new SQLiteConnection("Data Source=../../../database.sqlite");
+            string getTablesQuery =
+                "SELECT name FROM sqlite_master WHERE type ='table' AND name NOT LIKE 'sqlite_%';";
+            
+            List<string> tables = new List<string>();
+            SQLiteCommand getTables = new SQLiteCommand(getTablesQuery, Connection);
+            Connection.Open();
+            SQLiteDataReader reader = getTables.ExecuteReader();
+            while (reader.Read())
+                tables.Add(Convert.ToString(reader["name"]));
+            if (!tables.Contains("users"))
+            {
+                SQLiteCommand addUserTab = new SQLiteCommand(createUserTable, Connection);
+                addUserTab.ExecuteNonQuery();
+            }
+
+            if (!tables.Contains("workers"))
+            {
+                SQLiteCommand addWorkersTab = new SQLiteCommand(createWorkersTable, Connection);
+                addWorkersTab.ExecuteNonQuery();
+            }
+
+            Connection.Close();
         }
     }
 }
