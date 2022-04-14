83b44d41d9b45f911657f73bddc5116609be0c1a
Kabanosk <fiolkawojciech@gmail.com>
2022-04-14 14:01:33 +0200

    Add database handler class

---
+++ Database.cs
@@ -0,0 +1,20 @@
+﻿using System.Data.SQLite;
+using System.IO;
+
+namespace management_system
+{
+    public class Database
+    {
+        public SQLiteConnection Connection;
+
+        public Database()
+        {
+            if (!File.Exists("../../../database.sqlite"))
+            {
+                SQLiteConnection.CreateFile("../../../database.sqlite");
+            }
+
+            Connection = new SQLiteConnection("Data Source=../../../database.sqlite");
+        }
+    }
+}
