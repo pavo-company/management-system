e183936896e72f2bb3e31d233578f872a89b6696
Kabanosk <fiolkawojciech@gmail.com>
2022-04-22 22:11:09 +0200

    Some changes after debuging errors

--- src/Person.cs
+++ src/Person.cs
@@ -5,7 +5,7 @@ namespace management_system
 {
     public class Person
     {
-        public int Id { get; init; }
+        protected int Id { get; init; }
         protected string Name { get; init; }
         protected string Surname { get; init; }
         protected string Email { get; init; }
