62f00c874389a776052c7de5a0af1f5e944cdf7d
Kabanosk <fiolkawojciech@gmail.com>
2022-04-22 19:31:37 +0200

    Change class fields from set to init and remove toString funcktion

--- src/Person.cs
+++ src/Person.cs
@@ -1,14 +1,13 @@
-﻿namespace management_system
+﻿using System;
+using System.Data.SQLite;
+
+namespace management_system
 {
     public class Person
     {
-        protected int Id { get; set; }
-        protected string Name { get; set; }
-        protected string Surname { get; set; }
-        protected string Email { get; set; }
-        public override string ToString()
-        {
-            return string.Format("User:\n\tName: {0}\n\tEmail: {1}\n\tId: {2}", Name, Email, Id);
-        }
+        public int Id { get; init; }
+        protected string Name { get; init; }
+        protected string Surname { get; init; }
+        protected string Email { get; init; }
     }
 }
