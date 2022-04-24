a892f0ca787d8052540ec33046434b5a999fc76a
Kabanosk <fiolkawojciech@gmail.com>
2022-04-24 19:54:37 +0200

    Code cleaning

--- src/Person.cs
+++ src/Person.cs
@@ -5,7 +5,6 @@ namespace management_system
 {
     public class Person
     {
-        protected long Id { get; init; }
         protected string Name { get; init; }
         protected string Surname { get; init; }
         protected string Email { get; init; }
