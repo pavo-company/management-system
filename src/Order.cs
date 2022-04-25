c86dfe0776122ddb3ec276294a9cb2730ca9dbdc
Kabanosk <fiolkawojciech@gmail.com>
2022-04-25 23:50:05 +0200

    Add date and is_cyclic fields to orders table

--- src/Order.cs
+++ src/Order.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Data.SQLite;
+using System.Diagnostics.SymbolStore;
 
 namespace management_system
 {
@@ -9,19 +10,21 @@ namespace management_system
         private int ItemId { get; set; }
         private int Amount { get; set; }
         private string Date { get; set; }
+        private bool IsCyclic { get; set; }
 
-        public Order(int supplierId, int itemId, int amount)
+        public Order(int supplierId, int itemId, int amount, bool isCyclic)
         {
             SupplierId = supplierId;
             ItemId = itemId;
             Amount = amount;
             Date = DateTime.Today.ToString();
+            IsCyclic = isCyclic;
         }
         
         public void AddToDatabase(Database db)
         {
             string query = 
-                "INSERT INTO orders ('supplier_id', 'item_id', 'amount') VALUES (@supplier_id, @item_id, @amount)";
+                "INSERT INTO orders ('supplier_id', 'item_id', 'amount', 'date', 'is_cyclic') VALUES (@supplier_id, @item_id, @amount, @date, @is_cyclic)";
 
             SQLiteCommand command = new SQLiteCommand(query, db.Connection);
             SQLiteCommand backupCommand = new SQLiteCommand(query, db.BackupConnection);
@@ -36,6 +39,12 @@ namespace management_system
             
             command.Parameters.AddWithValue("@amount", Amount);
             backupCommand.Parameters.AddWithValue("@amount", Amount);
+            
+            command.Parameters.AddWithValue("@date", Date);
+            backupCommand.Parameters.AddWithValue("@date", Date);
+            
+            command.Parameters.AddWithValue("@is_cyclic", IsCyclic);
+            backupCommand.Parameters.AddWithValue("@is_cyclic", IsCyclic);
             
             command.ExecuteNonQuery();
             backupCommand.ExecuteNonQuery();
