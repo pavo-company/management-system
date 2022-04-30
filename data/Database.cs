5fec7aba27ff206ddab223ecd05ea99be8fcc0ff
zielinsky <MrZBiznes@gmail.com>
2022-04-30 20:16:16 +0200

    Code cleaning

--- data/Database.cs
+++ data/Database.cs
@@ -37,17 +37,7 @@ namespace management_system
             
             Migrations = new Migrations(Connection, BackupConnection);
             
-            if (tables.Contains("license"))
-            {
-                this.checkLicense();
-            }
-            
             Close();
-        }
-
-        private void checkLicense()
-        {
-            throw new NotImplementedException();
         }
 
         private static void CreateDatabaseIfNotExists()
